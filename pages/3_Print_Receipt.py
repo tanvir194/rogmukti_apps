7939dfc1d139a249113276e027f83ecdfc4dee8d
tanvir194 <157778188+tanvir194@users.noreply.github.com>
2026-06-13 11:07:23 +0600

    Update app.py
    Adjusted receipt positioning and width for better fit.

--- pages/3_Print_Receipt.py
+++ pages/3_Print_Receipt.py
@@ -96,9 +96,9 @@ st.markdown("""
         .receipt-container {
             position: absolute !important;
             left: -30% !important;
-            top: -40% !important;
+            top: -100% !important;
             width: 100% !important;
-            max-width: 105% !important;
+            max-width: 115% !important;
             box-shadow: none !important;
             border: 1px solid #000000 !important;
             padding: 10px !important;
